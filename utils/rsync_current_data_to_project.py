08f43d6f6578c04e85bbbc0f9eafac6671bb3517
Brian Belcher <belcher.bn@gmail.com>
2021-11-19 10:19:46 -0500

    comments

--- utils/rsync_current_data_to_project.py
+++ utils/rsync_current_data_to_project.py
@@ -1,3 +1,7 @@
+'''Sync forecast data
+
+	Sync forecast data from data2 to development location.
+'''
 import os
 
 cmd = 'rsync -a bnb2@cool001.eas.cornell.edu:/pool/data2/bnb2/ny_runoff_risk/web_data/fcst_map_images /Users/bnb2/projects/ny-applicator-forecast/public/.'
